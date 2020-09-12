414a3c8de131c61cb5895f41bf0b3d9fe226faff
Mahbub126 <38081563+Mahbub126@users.noreply.github.com>
2020-09-13 04:15:18 +1000

    Add files via upload

---
+++ Demo.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIDemo
+{
+    public class Demo<T>
+    {
+        public ListOfUsersDTO getUsers(string endpoint)
+        {
+            var user = new APIHelper<ListOfUsersDTO>();
+            var url = user.SetUrl(endpoint);
+            var request = user.CreateGetRequest();
+            var response = user.GetResponse(url, request);
+            ListOfUsersDTO content = user.getContent<ListOfUsersDTO>(response);
+            return content;
+
+        }
+        public CreateUserDTO CreateUser(string endpoint, dynamic payload)
+        {
+
+        var user = new APIHelper<CreateUserDTO>();
+        var url = user.SetUrl(endpoint);
+        var jsonReq = user.Serialize(payload);
+        var request = user.CreatePostRequest(jsonReq);
+        var response = user.GetResponse(url, request);
+        CreateUserDTO content = user.getContent<CreateUserDTO>(response);
+            return content;
+        }
+    }
+}
